4c696bddddaceef56f15e258a79489a2023214dc
Mathilde Brosseau <mathilde.brosseau@polymtl.ca>
2024-02-21 13:50:00 -0500

    Added translation for new dialog and tab

--- i18n/transition_qgis_fr.ts
+++ i18n/transition_qgis_fr.ts
@@ -16,7 +16,7 @@
     <message>
         <location filename="../create_route.py" line="50"/>
         <source>Arrival time</source>
-        <translation>Heure d&apos;arrivée [HH:MM]</translation>
+        <translation>Heure d'arrivée [HH:MM]</translation>
     </message>
     <message>
         <location filename="../create_route.py" line="55"/>
@@ -26,7 +26,7 @@
     <message>
         <location filename="../create_route.py" line="59"/>
         <source>Minimum waiting time (minutes)</source>
-        <translation>Temps minimum d&apos;attente (minutes)</translation>
+        <translation>Temps minimum d'attente (minutes)</translation>
     </message>
     <message>
         <location filename="../create_route.py" line="63"/>
@@ -36,7 +36,7 @@
     <message>
         <location filename="../create_route.py" line="65"/>
         <source>Maximum access and egress travel time (minutes)</source>
-        <translation>Temps maximum d&apos;accès à l&apos;origine et à destination (minutes)</translation>
+        <translation>Temps maximum d'accès à l'origine et à destination (minutes)</translation>
     </message>
     <message>
         <location filename="../create_route.py" line="75"/>
@@ -46,17 +46,17 @@
     <message>
         <location filename="../create_route.py" line="71"/>
         <source>Maximum access travel time when transferring (minutes)</source>
-        <translation>Temps maximum d&apos;accès lors des transferts (minutes)</translation>
+        <translation>Temps maximum d'accès lors des transferts (minutes)</translation>
     </message>
     <message>
         <location filename="../create_route.py" line="77"/>
         <source>Maximum first waiting time (minutes)</source>
-        <translation>Temps d&apos;attente maximal au premier arrêt (minutes)</translation>
+        <translation>Temps d'attente maximal au premier arrêt (minutes)</translation>
     </message>
     <message>
         <location filename="../create_route.py" line="80"/>
         <source>If waiting time at first stop is greater than this value for a line, ignore the departure of this line at this stop</source>
-        <translation>Si l&apos;attente au premier arrêt est plus grande que cette valeur pour une ligne, ignorer le départ de cette ligne à cet arrêt</translation>
+        <translation>Si l'attente au premier arrêt est plus grande que cette valeur pour une ligne, ignorer le départ de cette ligne à cet arrêt</translation>
     </message>
     <message>
         <location filename="../create_route.py" line="82"/>
@@ -65,16 +65,34 @@
     </message>
 </context>
 <context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../login_dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished">Se connecter</translation>
+    </message>
+    <message>
+        <location filename="../login_dialog.ui" line="26"/>
+        <source>Password</source>
+        <translation>Mot de passe</translation>
+    </message>
+    <message>
+        <location filename="../login_dialog.ui" line="39"/>
+        <source>Username or email</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>Transition</name>
     <message>
         <location filename="../transition_qgis.py" line="235"/>
         <source>&amp;Transition</source>
-        <translation>&amp;Transition</translation>
+        <translation type="obsolete">&amp;Transition</translation>
     </message>
     <message>
         <location filename="../transition_qgis.py" line="202"/>
         <source>Transition</source>
-        <translation>Transition</translation>
+        <translation type="obsolete">Transition</translation>
     </message>
 </context>
 <context>
@@ -92,27 +110,50 @@
     <message>
         <location filename="../transition_qgis_dockwidget_base.ui" line="42"/>
         <source>Send API request</source>
-        <translation>Envoyer requête API</translation>
+        <translation type="obsolete">Envoyer requête API</translation>
     </message>
     <message>
-        <location filename="../transition_qgis_dockwidget_base.ui" line="65"/>
+        <location filename="../transition_qgis_dockwidget_base.ui" line="52"/>
         <source>Reset</source>
         <translation>Réinitialiser</translation>
     </message>
     <message>
-        <location filename="../transition_qgis_dockwidget_base.ui" line="83"/>
+        <location filename="../transition_qgis_dockwidget_base.ui" line="96"/>
         <source>Create new route</source>
         <translation>Calculer un chemin</translation>
     </message>
     <message>
-        <location filename="../transition_qgis_dockwidget_base.ui" line="96"/>
+        <location filename="../transition_qgis_dockwidget_base.ui" line="109"/>
         <source>Capture From coord</source>
-        <translation>Choisir l&apos;origine</translation>
+        <translation>Choisir l'origine</translation>
     </message>
     <message>
-        <location filename="../transition_qgis_dockwidget_base.ui" line="129"/>
+        <location filename="../transition_qgis_dockwidget_base.ui" line="142"/>
         <source>Capture To coord</source>
         <translation>Choisir la destination</translation>
+    </message>
+    <message>
+        <location filename="../transition_qgis_dockwidget_base.ui" line="65"/>
+        <source>Get Transition nodes</source>
+        <translation>Obtenir les noeuds Transition</translation>
+    </message>
+    <message>
+        <location filename="../transition_qgis_dockwidget_base.ui" line="78"/>
+        <source>Get Transition paths</source>
+        <translation>Obtenir les chemins Transition</translation>
+    </message>
+</context>
+<context>
+    <name>TransitionWidget</name>
+    <message>
+        <location filename="../transition_qgis.py" line="239"/>
+        <source>&amp;Transition</source>
+        <translation>&amp;Transition</translation>
+    </message>
+    <message>
+        <location filename="../transition_qgis.py" line="206"/>
+        <source>Transition</source>
+        <translation>Transition</translation>
     </message>
 </context>
 </TS>

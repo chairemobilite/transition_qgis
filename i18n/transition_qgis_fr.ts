fa761edd9ebddb1c4e3c9f3fe118626f89f55c39
Mathilde Brosseau <mathilde.brosseau@polymtl.ca>
2024-04-12 11:13:52 -0400

    Fix translation of warnings

--- i18n/transition_qgis_fr.ts
+++ i18n/transition_qgis_fr.ts
@@ -4,195 +4,190 @@
 <context>
     <name>CreateAccessibilityForm</name>
     <message>
-        <location filename="../create_accessibility.py" line="28"/>
+        <location filename="../create_accessibility.py" line="39"/>
         <source>Time to use</source>
         <translation>Heure à utiliser</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="29"/>
+        <location filename="../create_accessibility.py" line="40"/>
         <source>Departure</source>
         <translation>Départ</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="31"/>
+        <location filename="../create_accessibility.py" line="42"/>
         <source>Arrival</source>
         <translation>Arrivée</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="40"/>
+        <location filename="../create_accessibility.py" line="51"/>
         <source>Departure or arrival time</source>
         <translation>Heure de départ ou d&apos;arrivée</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="45"/>
+        <location filename="../create_accessibility.py" line="56"/>
         <source>Number of polygons to calculate</source>
         <translation>Nombre de polygones à calculer</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="49"/>
+        <location filename="../create_accessibility.py" line="60"/>
         <source>A total duration of 60 minutes with a number of polygons of 3 implies that we will obtain three polygons: accessibility at 20 minutes, 40 minutes and 60 minutes.</source>
         <translation>Une durée de parcours de 60 minutes et un nombre de polygones de 3 implique que l&apos;on obtientra trois polygones: accès à 20 minutes, accès à 40 minutes et accès à 60 minutes.</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="55"/>
+        <location filename="../create_accessibility.py" line="66"/>
         <source>Delta (minutes)</source>
         <translation>Delta (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="60"/>
+        <location filename="../create_accessibility.py" line="71"/>
         <source>Delta interval (minutes)</source>
         <translation>Intervalle du delta (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="64"/>
+        <location filename="../create_accessibility.py" line="75"/>
         <source>A delta of 15 minutes with an interval of 5 minutes implies that the polygon area will be averaged over polygons obtained 15 minutes before, 10 minutes before, 5 minutes before, at specified time, 5 minutes after, 10 minutes after and 15 minutes after the specified departure or arrival time.</source>
         <translation>Un delta de 15 minutes avec intervalle de 5 minutes implique que la surface du polygone sera une moyenne des polygones obtenus à 15 minutes avant, 10 minutes avant, 5 minutes avant, à l&apos;heure spécifiée, 5 minutes après, 10 minutes après et 15 minutes après l&apos;heure de départ ou d&apos;arrivée choisie.</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="66"/>
+        <location filename="../create_accessibility.py" line="77"/>
         <source>Scenario</source>
         <translation>Scénario</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="71"/>
-        <source>Place name</source>
-        <translation type="obsolete">Nom du lieu</translation>
-    </message>
-    <message>
-        <location filename="../create_accessibility.py" line="75"/>
+        <location filename="../create_accessibility.py" line="86"/>
         <source>Maximum total travel time including access and egress (minutes)</source>
         <translation>Temps de parcours maximal incluant les accès (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="79"/>
+        <location filename="../create_accessibility.py" line="90"/>
         <source>Minimum waiting time (minutes)</source>
         <translation>Temps minimum d&apos;attente (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="83"/>
+        <location filename="../create_accessibility.py" line="94"/>
         <source>To account for timetable uncertainty, this value should be greater or equal to 1 minute. Suggested value: 3 minutes</source>
         <translation>Pour tenir compte des incertitudes des horaires, cette valeur doit être de 1 minute ou plus. Valeur suggérée: 3 minutes</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="85"/>
+        <location filename="../create_accessibility.py" line="96"/>
         <source>Maximum access and egress travel time (minutes)</source>
         <translation>Temps maximum d&apos;accès à l&apos;origine et à destination (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="95"/>
+        <location filename="../create_accessibility.py" line="106"/>
         <source>To avoid long calculation time, this value has a maximum of 20 minutes.</source>
         <translation>Pour éviter de longs temps de calcul, la valeur maximale est de 20 minutes.</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="91"/>
+        <location filename="../create_accessibility.py" line="102"/>
         <source>Maximum access travel time when transferring (minutes)</source>
         <translation>Temps maximum d&apos;accès lors des transferts (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="97"/>
+        <location filename="../create_accessibility.py" line="108"/>
         <source>Maximum first waiting time (minutes)</source>
         <translation>Temps d&apos;attente maximal au premier arrêt (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="100"/>
-        <source>If waiting time at first stop is greater than this value for a line, ignore the departure of this line at this stop</source>
-        <translation>Si l&apos;attente au premier arrêt est plus grande que cette valeur pour une ligne, ignorer le départ de cette ligne à cet arrêt</translation>
-    </message>
-    <message>
-        <location filename="../create_accessibility.py" line="102"/>
+        <location filename="../create_accessibility.py" line="114"/>
         <source>Walking speed (km/h)</source>
         <translation>Vitesse de marche (km/h)</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="51"/>
+        <location filename="../create_accessibility.py" line="62"/>
         <source>Display polygons in distinct layers</source>
         <translation>Afficher les polygones dans des couches différentes</translation>
     </message>
     <message>
-        <location filename="../create_accessibility.py" line="72"/>
+        <location filename="../create_accessibility.py" line="83"/>
         <source>Accessibility map name</source>
         <translation>Nom de la carte d&apos;accessibilité</translation>
+    </message>
+    <message>
+        <location filename="../create_accessibility.py" line="112"/>
+        <source>If waiting time at first stop is greater than this value for a line, ignore the departure of this line at this stop. Use -1 to ignore this field and allow for indefinite waiting.</source>
+        <translation>Si l&apos;attente au premier arrêt est plus grande que cette valeur pour une ligne, ignorer le départ de cette ligne à cet arrêt. Utilisez -1 pour ignorer ce champ et accepter l&apos;attente indéfinie.</translation>
     </message>
 </context>
 <context>
     <name>CreateRouteDialog</name>
     <message>
-        <location filename="../create_route.py" line="31"/>
+        <location filename="../create_route.py" line="40"/>
         <source>Calculate for the following modes</source>
         <translation>Effectuer le calcul de chemin pour les modes suivants</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="36"/>
+        <location filename="../create_route.py" line="45"/>
         <source>Time to use</source>
         <translation>Heure à utiliser</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="37"/>
+        <location filename="../create_route.py" line="46"/>
         <source>Departure</source>
         <translation>Départ</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="39"/>
+        <location filename="../create_route.py" line="48"/>
         <source>Arrival</source>
         <translation>Arrivée</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="48"/>
+        <location filename="../create_route.py" line="57"/>
         <source>Departure or arrival time</source>
         <translation>Heure de départ ou d&apos;arrivée [HH:MM]</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="53"/>
+        <location filename="../create_route.py" line="62"/>
         <source>Maximum total travel time including access and egress (minutes)</source>
         <translation>Temps de parcours maximal incluant les accès (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="58"/>
+        <location filename="../create_route.py" line="67"/>
         <source>Minimum waiting time (minutes)</source>
         <translation>Temps minimum d&apos;attente (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="62"/>
+        <location filename="../create_route.py" line="71"/>
         <source>To account for timetable uncertainty, this value should be greater or equal to 1 minute. Suggested value: 3 minutes</source>
         <translation>Pour tenir compte des incertitudes des horaires, cette valeur doit être de 1 minute ou plus. Valeur suggérée: 3 minutes</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="64"/>
+        <location filename="../create_route.py" line="73"/>
         <source>Maximum access and egress travel time (minutes)</source>
         <translation>Temps maximum d&apos;accès à l&apos;origine et à destination (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="74"/>
+        <location filename="../create_route.py" line="83"/>
         <source>To avoid long calculation time, this value has a maximum of 20 minutes.</source>
         <translation>Pour éviter de longs temps de calcul, la valeur maximale est de 20 minutes.</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="70"/>
+        <location filename="../create_route.py" line="79"/>
         <source>Maximum access travel time when transferring (minutes)</source>
         <translation>Temps maximum d&apos;accès lors des transferts (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="76"/>
+        <location filename="../create_route.py" line="85"/>
         <source>Maximum first waiting time (minutes)</source>
         <translation>Temps d&apos;attente maximal au premier arrêt (minutes)</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="79"/>
+        <location filename="../create_route.py" line="88"/>
         <source>If waiting time at first stop is greater than this value for a line, ignore the departure of this line at this stop</source>
         <translation>Si l&apos;attente au premier arrêt est plus grande que cette valeur pour une ligne, ignorer le départ de cette ligne à cet arrêt</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="81"/>
+        <location filename="../create_route.py" line="90"/>
         <source>Scenario</source>
         <translation>Scénario</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="87"/>
+        <location filename="../create_route.py" line="96"/>
         <source>Calculate with alternatives</source>
         <translation>Calcul avec alternatives</translation>
     </message>
     <message>
-        <location filename="../create_route.py" line="91"/>
+        <location filename="../create_route.py" line="100"/>
         <source>Route name</source>
         <translation>Nom du chemin</translation>
     </message>
@@ -200,12 +195,12 @@
 <context>
     <name>CreateSettingsForm</name>
     <message>
-        <location filename="../create_settings.py" line="21"/>
+        <location filename="../create_settings.py" line="37"/>
         <source>Username or email</source>
         <translation>Nom d&apos;utilisateur ou courriel</translation>
     </message>
     <message>
-        <location filename="../create_settings.py" line="30"/>
+        <location filename="../create_settings.py" line="46"/>
         <source>Transition server URL</source>
         <translation>URL du serveur Transition</translation>
     </message>
@@ -241,22 +236,22 @@
 <context>
     <name>LoginDialog</name>
     <message>
-        <location filename="../create_login.py" line="29"/>
+        <location filename="../create_login.py" line="51"/>
         <source>Invalid loggin credentials</source>
         <translation>Identifiants de connexion invalides</translation>
     </message>
     <message>
-        <location filename="../create_login.py" line="29"/>
+        <location filename="../create_login.py" line="51"/>
         <source>Please enter your username and password.</source>
         <translation>Veuillez entrer votre nom d&apos;utilisateur ou votre courriel.</translation>
     </message>
     <message>
-        <location filename="../create_login.py" line="44"/>
+        <location filename="../create_login.py" line="66"/>
         <source>Unable to connect to server</source>
         <translation>Impossible de se connecter au serveur</translation>
     </message>
     <message>
-        <location filename="../create_login.py" line="44"/>
+        <location filename="../create_login.py" line="66"/>
         <source>Unable to connect to your Transition server.
 Make sure you provided the right server URL and that the server is up.</source>
         <translation>Impossible de se connecter à votre serveur Transition.
@@ -341,34 +336,34 @@ d&apos;accessibilité</translation>
 <context>
     <name>TransitionWidget</name>
     <message>
-        <location filename="../transition_qgis.py" line="241"/>
+        <location filename="../transition_qgis.py" line="211"/>
         <source>&amp;Transition</source>
         <translation>&amp;Transition</translation>
     </message>
     <message>
-        <location filename="../transition_qgis.py" line="202"/>
+        <location filename="../transition_qgis.py" line="188"/>
         <source>Transition</source>
         <translation>Transition</translation>
     </message>
     <message>
-        <location filename="../transition_qgis.py" line="338"/>
+        <location filename="../transition_qgis.py" line="304"/>
         <source>Unable to connect to server</source>
         <translation>Imposible de se connecter au serveur</translation>
     </message>
     <message>
-        <location filename="../transition_qgis.py" line="338"/>
+        <location filename="../transition_qgis.py" line="304"/>
         <source>Unable to connect to your Transition server.
 Make sure you provided the right server URL and that the server is up.</source>
         <translation>Impossible de se connecter à votre serveur Transition.
 Assurez-vous d&apos;avoir fourni la bonne URL du serveur et que le serveur est actif.</translation>
     </message>
     <message>
-        <location filename="../transition_qgis.py" line="389"/>
+        <location filename="../transition_qgis.py" line="348"/>
         <source>No modes selected</source>
         <translation>Aucun mode sélectionné</translation>
     </message>
     <message>
-        <location filename="../transition_qgis.py" line="389"/>
+        <location filename="../transition_qgis.py" line="348"/>
         <source>Please select at least one mode.</source>
         <translation>Veuillez sélectionner au moins un mode.</translation>
     </message>
